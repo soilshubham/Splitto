c3bd62b0a4e34fcba42870f79f857a8d45b806ea
Sahil Shubham <soilshubham@gmail.com>
2022-04-26 14:10:54 +0530

    Initial layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>home</div>} />
-        <Route path="about" element={<div>about</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
