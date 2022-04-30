e7c9a522d58e3093c3d429becaa0e5e456c6fac2
Sahil Shubham <soilshubham@gmail.com>
2022-04-30 16:49:21 +0530

    Added login and register page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -8,13 +8,14 @@ const Login = () => {
             <Navbar />
             <div className="flex flex-col justify-center items-center mt-10 z-10 ">
                 <form className="flex flex-col justify-center items-center w-[23rem] rounded-lg p-7 shadow-2xl">
-                    <p className='text-center font-bold text-4xl md:text-2xl'>Login</p>
+                    <p className='text-center font-bold text-4xl md:text-2xl'>Register</p>
                     <div className="flex flex-col max-w-2xl w-full mt-10 gap-5">
+                        <input type="text" className="bg-slate-100 p-4 min-w-full rounded-lg" placeholder='Username' />
                         <input type="email" className="bg-slate-100 p-4 min-w-full rounded-lg" placeholder='Email' />
                         <input type="password" className="bg-slate-100 p-4 min-w-full rounded-lg" placeholder='Password' />
                         <button className="bg-color3 p-4 min-w-full rounded-lg text-white max-w-max">Login</button>
                     </div>
-                    <a className="text-sm mt-4 hover:text-sky-700 text-stone-600" href="">Create a new account?</a>
+                    <a className="text-sm mt-4 hover:text-sky-700 text-stone-600" href="">Already have an account?</a>
                 </form>
             </div>
         </div>
